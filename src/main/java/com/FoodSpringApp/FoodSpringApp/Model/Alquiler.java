dc44765dbe002f95cba348b7f2dd2135761f2f2f
cbravose <cbravose@uoc.edu>
2024-11-01 10:47:09 +0100

    se añade clase para manejo vehiculos

--- src/main/java/com/FoodSpringApp/FoodSpringApp/Model/Alquiler.java
+++ src/main/java/com/FoodSpringApp/FoodSpringApp/Model/Alquiler.java
@@ -1,7 +1,15 @@
 package com.FoodSpringApp.FoodSpringApp.model;
 
 import java.util.Date;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 @Entity
 @Table(name = "alquiler")
---
+++ src/main/java/com/FoodSpringApp/FoodSpringApp/service/VehiculoService.java
@@ -0,0 +1,20 @@
+package com.FoodSpringApp.FoodSpringApp.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.FoodSpringApp.FoodSpringApp.model.Vehiculo;
+import com.FoodSpringApp.FoodSpringApp.repository.VehiculoRepository;
+
+@Service
+public class VehiculoService{
+
+    @Autowired
+    private VehiculoRepository  vehiculoRepository;
+
+    public List<Vehiculo> obtenerTodosAlquileres() {
+        return vehiculoRepository.findAll();
+    }
+}
