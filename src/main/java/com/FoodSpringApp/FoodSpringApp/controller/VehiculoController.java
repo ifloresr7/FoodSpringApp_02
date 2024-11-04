f8f9fb8fd3333e37862afbf2e88bfda17901b70b
cbravose <cbravose@uoc.edu>
2024-11-04 19:22:56 +0100

    vehiculos root

--- src/main/java/com/FoodSpringApp/FoodSpringApp/controller/VehiculoController.java
+++ src/main/java/com/FoodSpringApp/FoodSpringApp/controller/VehiculoController.java
@@ -1,5 +1,58 @@
 package com.FoodSpringApp.FoodSpringApp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.FoodSpringApp.FoodSpringApp.model.Vehiculo;
+import com.FoodSpringApp.FoodSpringApp.service.VehiculoService;
+
+@Controller
+@RequestMapping("/vehiculos")
 public class VehiculoController {
     
+
+
+     @Autowired
+    private VehiculoService vehiculoService;
+
+     
+    @GetMapping("/nuevo")
+    public String mostrarFormularioCreacion(Model model) {
+        model.addAttribute("vehiculo", new Vehiculo());
+        return "formularioCrearVehiculo";  
+    }
+ 
+    @PostMapping("/guardar")
+    public String guardarVehiculo(@ModelAttribute Vehiculo vehiculo) {
+        vehiculoService.save(vehiculo);  
+        return "redirect:/vehiculos";  
+    }
+
+    
+    @GetMapping("/editar/{id}")
+    public String mostrarFormularioEdicion(@PathVariable int id, Model model) {
+        Vehiculo vehiculo = vehiculoService.findById(id);
+        model.addAttribute("vehiculo", vehiculo);
+        return "formularioEditarVehiculo"; // Nombre de la plantilla Thymeleaf para editar
+    }
+
+ 
+    @PostMapping("/actualizar")
+    public String actualizarVehiculo(@ModelAttribute Vehiculo vehiculo) {
+        vehiculoService.save(vehiculo);
+        return "redirect:/vehiculos"; 
+    }
+
+    
+    @GetMapping("/eliminar/{id}")
+    public String eliminarVehiculo(@PathVariable int id) {
+        vehiculoService.deleteById(id);
+        return "redirect:/vehiculos";  
+
 }
--- src/main/java/com/FoodSpringApp/FoodSpringApp/service/VehiculoService.java
+++ src/main/java/com/FoodSpringApp/FoodSpringApp/service/VehiculoService.java
@@ -17,4 +17,53 @@ public class VehiculoService{
     public List<Vehiculo> obtenerTodosVehiculos() {
         return vehiculoRepository.findAll();
     }
+    /**
+     * para actualizar el vehiculo
+     * @param vehiculo
+     * @return
+     */
+    public Vehiculo save(Vehiculo vehiculo) {
+        return vehiculoRepository.save(vehiculo);
+    }
+
+
+    public Vehiculo findById(int id) {
+        return vehiculoRepository.findById(id).orElse(null);
+    }
+
+
+    /**
+     * 
+     * @param id  es id o Pk id unico
+     * @param autonomia_km  autonomia en km 
+     * @param color  es un string para color
+     * @param marca  es un string para la marca
+     * @param potencia_cv es un int para la potencia
+     * @param puertas es un int para las pueras
+     * @param actualizaVehiculo  es el "@Sutowired" VehiculoRepository  vehiculoRepository
+     * @return
+     */
+    public Vehiculo actualizarVehiculo(Integer id, int autonomia_km, String color, String marca, int potencia_cv, int puertas,  Vehiculo actualizaVehiculo) {
+
+        if (vehiculoRepository.existsById(id) )  // recibe id si existe lo modifica
+        {
+            actualizaVehiculo.setAutonomia_km(autonomia_km); 
+            actualizaVehiculo.setColor(color);
+            actualizaVehiculo.setMarca(marca);
+            actualizaVehiculo.setPotencia_cv(potencia_cv);
+            actualizaVehiculo.setPuertas(puertas);
+
+            return vehiculoRepository.save(actualizaVehiculo);
+        }
+        return null; // O lanza una excepción si no existe
+    }
+
+    /**
+     * por su id si existe se borra
+     * @param id
+     */
+    public void deleteById(int id) {
+        vehiculoRepository.deleteById(id);
+    }
+    
 }
