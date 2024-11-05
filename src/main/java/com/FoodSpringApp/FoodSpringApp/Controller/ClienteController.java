c681512918728a9b707450784dd9d913aa4479eb
cmoticeac <147082946+cmoticeac@users.noreply.github.com>
2024-11-05 22:13:20 +0100

    modales

--- src/main/java/com/FoodSpringApp/FoodSpringApp/Controller/ClienteController.java
+++ src/main/java/com/FoodSpringApp/FoodSpringApp/Controller/ClienteController.java
@@ -1,12 +1,15 @@
 package com.FoodSpringApp.FoodSpringApp.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.FoodSpringApp.FoodSpringApp.model.Cliente;
@@ -18,34 +21,11 @@ public class ClienteController {
 
     @Autowired
     private ClienteService clienteService;
-
-    // Mostrar formulario de creación de cliente
-    @GetMapping("/nuevo")
-    public String mostrarFormularioCreacion(Model model) {
-        model.addAttribute("cliente", new Cliente());
-        return "formularioCrearCliente"; // Nombre de la plantilla Thymeleaf
-    }
-
-    // Guardar el cliente enviado desde el formulario y redirigir a la lista de clientes
-    @PostMapping("/guardar")
-    public String guardarCliente(@ModelAttribute Cliente cliente) {
-        clienteService.save(cliente); // Guardar el cliente en la base de datos
-        return "redirect:/clientes"; // Redirige a la lista de clientes
-    }
-
-    // Mostrar formulario de edición de cliente
-    @GetMapping("/editar/{id}")
-    public String mostrarFormularioEdicion(@PathVariable int id, Model model) {
-        Cliente cliente = clienteService.findById(id);
-        model.addAttribute("cliente", cliente);
-        return "formularioEditarCliente"; // Nombre de la plantilla Thymeleaf para editar
-    }
-
-    // Actualizar un cliente existente y redirigir a la lista de clientes
-    @PostMapping("/actualizar")
-    public String actualizarCliente(@ModelAttribute Cliente cliente) {
-        clienteService.save(cliente);
-        return "redirect:/clientes"; // Redirige a la lista de clientes
+    
+    @GetMapping
+    public ResponseEntity<List<Cliente>> obtenerTodosClientes() {
+    List<Cliente> clientes = clienteService.obtenerTodosClientes();
+    return ResponseEntity.ok(clientes);
     }
 
     // Eliminar un cliente y redirigir a la lista de clientes
@@ -54,4 +34,17 @@ public class ClienteController {
         clienteService.deleteById(id);
         return "redirect:/clientes"; // Redirige a la lista de clientes
     }
+
+    @PostMapping
+    public ResponseEntity<Cliente> guardarCliente(@RequestBody Cliente cliente) {
+    Cliente nuevoCliente = clienteService.save(cliente);
+    return ResponseEntity.ok(nuevoCliente);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> actualizarCliente(@PathVariable int id, @RequestBody Cliente clienteData) {
+    Cliente clienteActualizado = clienteService.update(id, clienteData);
+    return clienteActualizado != null ? ResponseEntity.ok(clienteActualizado) : ResponseEntity.notFound().build();
+    }
+
 }
